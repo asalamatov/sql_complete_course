93fbe43a6f913537071c587a4c8605da6287da5c
Azamat Salamatov <azamat.salamatov2004@gmail.com>
2023-03-19 14:42:59 -0500

    more flexible get_clients_by_state procedure

--- stored_procedures/get_clients_by_state.sql
+++ stored_procedures/get_clients_by_state.sql
@@ -1,13 +1,1 @@
-DROP PROCEDURE IF EXISTS get_clients_by_state;
-
-DELIMITER $$
-CREATE PROCEDURE get_clients_by_state
-(	
-	state CHAR(2)
-)
-BEGIN
-	SELECT * FROM clients c
-    WHERE c.state = state;
-END$$
-
-DELIMITER ;
+call sql_invoicing.get_clients_by_state('CA');
